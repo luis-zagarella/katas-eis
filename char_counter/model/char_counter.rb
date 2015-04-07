e85de399fd9bab6be87c0564b8f8080e7f6ff87f
luis-zagarella <zagarojo23@gmail.com>
2015-04-07 18:51:31 -0300

    todos los test ok

--- char_counter/model/char_counter.rb
+++ char_counter/model/char_counter.rb
@@ -1,7 +1,10 @@
 class CharCounter
 	
 	def self.count(un_string)
-		return []
-	end
-
+		res = Hash.new	
+		if (un_string != nil) 
+			un_string = un_string.delete(" ")
+			un_string.each_char{|c| res[c] = un_string.count(c)}
+		end		
+		res
 end
