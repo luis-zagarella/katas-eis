34a9e142297ca88194e992a6b4ce4d594f1846c3
luis-zagarella <zagarojo23@gmail.com>
2015-04-27 03:41:48 -0300

    primer test andando

--- tenis/model/marcador.rb
+++ tenis/model/marcador.rb
@@ -1,3 +1,21 @@
 class Marcador
 
+	def initialize
+		@puntaje_puntos = [0,0]
+		@puntaje_games = [0,0]
+		@puntaje_sets = [0,0]
+	end
+
+	def puntaje_puntos 
+		@puntaje_puntos	
+	end
+ 	
+	def puntaje_games
+		@puntaje_games 
+	end
+		
+	def puntaje_sets 
+		@puntaje_sets 
+	end
+
 end
--- tenis/spec/marcador_spec.rb
+++ tenis/spec/marcador_spec.rb
@@ -3,4 +3,15 @@ require_relative '../model/marcador.rb'
 
 describe 'Marcador' do
 
+  describe 'initialize' do
+
+	it 'el partido deberia comenzar en 0' do
+		marcador = Marcador.new
+		marcador.puntaje_puntos.should eq [0,0]
+ 		marcador.puntaje_games.should eq [0,0]
+		marcador.puntaje_sets.should eq [0,0]
+	end	
+ 
+  end
+     
 end
