4fa4cbf6945e2a4b4815d2068e58fc840e839aa1
luis-zagarella <zagarojo23@gmail.com>
2015-05-05 20:24:42 -0300

    Feature create ships pasa

--- batalla_naval/features/step_definitions/battleship_steps.rb
+++ batalla_naval/features/step_definitions/battleship_steps.rb
@@ -1,13 +1,23 @@
 require_relative '../../app/models/board.rb'
+require_relative '../../app/models/point.rb'
+require_relative '../../app/models/ship.rb'
 
-Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |arg1, arg2|
-  @board = Board.new arg1, arg2
+Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |width, long|
+  @board = Board.new width.to_i, long.to_i
 end
 
-Given(/^I create a small ship in position "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I create a small ship in position "(.*?)"$/) do |point|	
+  a = point.split(":")
+  x= a[0].to_i
+  y= a[1].to_i
+  @origin = Point.new(x,y)
+  @board.add_a_ship_to_fleet('small', @origin,'North')
 end
 
-Then(/^position "(.*?)" is not empty$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^position "(.*?)" is not empty$/) do |point|
+   b = point.split(":")
+   	x1= b[0].to_i
+   	y1= b[1].to_i
+   @my_point = Point.new(x1,y1) 
+   expect(@board.is_empty(@my_point)).to eq false
 end
