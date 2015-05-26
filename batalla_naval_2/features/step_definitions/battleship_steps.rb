53a137acec6747379e16a5ed181f2a352ace0bb0
luis-zagarella <zagarojo23@gmail.com>
2015-05-26 08:22:10 -0300

    Feature agregar barcos - Corriendo todos los steps del feature

--- batalla_naval_2/features/step_definitions/battleship_steps.rb
+++ batalla_naval_2/features/step_definitions/battleship_steps.rb
@@ -1,25 +1,51 @@
 require_relative '../../app/models/board.rb'
+require_relative '../../app/models/point.rb'
+require_relative '../../app/models/ship.rb'
 
-Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |arg1, arg2|
-  @board = Board.new arg1, arg2
+Given(/^a board with dimensions "(.*?)" x "(.*?)"$/) do |width, long|
+  @board = Board.new width.to_i, long.to_i
 end
 
-Given(/^I create a small ship in position "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given(/^I create a small ship in position "(.*?)"$/) do |point|	
+  a = point.split(":")
+  x= a[0].to_i
+  y= a[1].to_i
+  @origin = Point.new(x,y)
+  begin
+    @board.add_a_ship_to_fleet('small', @origin,'North')
+  rescue RuntimeError => @an_error	
+  end
 end
 
-Then(/^position "(.*?)" is not empty$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^position "(.*?)" is not empty$/) do |point|
+  a = point.split(":")
+  x= a[0].to_i
+  y= a[1].to_i
+  @my_point = Point.new(x,y) 
+  expect(@board.is_empty(@my_point)).to eq false
 end
 
-When(/^I create a large ship in position "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I create a large ship in position "(.*?)"$/) do |point|
+  a = point.split(":")
+  x= a[0].to_i
+  y= a[1].to_i
+  @origin = Point.new(x,y)
+  begin
+    @board.add_a_ship_to_fleet('large', @origin,'North')
+  rescue RuntimeError => @an_error
+  end
 end
 
-Then(/^position "(.*?)" and one more position are not empty$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^position "(.*?)" and one more position are not empty$/) do |point|
+  a = point.split(":")
+  x= a[0].to_i
+  y= a[1].to_i
+  @my_point = Point.new(x,y) 
+  @neighbor1 = @my_point.next_at('North')
+  expect(@board.is_empty(@my_point)).to eq false
+  expect(@board.is_empty(@neighbor1)).to eq false
 end
 
-Then(/^it should raise error "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then(/^it should raise error "(.*?)"$/) do |msj|
+  expect{ raise @an_error }.to raise_error(msj)
 end
