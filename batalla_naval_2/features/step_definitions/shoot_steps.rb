f74621e069301463c9f08d3d314b9a3f27097bee
luis-zagarella <zagarojo23@gmail.com>
2015-05-26 10:00:21 -0300

    Feature disparos - Todos los steps andando - Fin de la kata 2

--- batalla_naval_2/features/step_definitions/shoot_steps.rb
+++ batalla_naval_2/features/step_definitions/shoot_steps.rb
@@ -7,22 +7,21 @@ Given(/^a large ship in position: “(\d+):(\d+)”$/) do |x, y|
   @board.add_a_ship_to_fleet('large', @origin,'North')
 end
 
-When(/^I shoot to position “(\d+):(\d+)”$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+When(/^I shoot to position “(\d+):(\d+)”$/) do |x, y|
+  begin
+    @response = @board.shoot(Point.new(x.to_i,y.to_i))
+  rescue RuntimeError => @an_error
+  end
 end
 
 Then(/^I get hit$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@response).to eq 'hit'
 end
 
 Then(/^I get water$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^I shoot to position “(\d+):(\d+)”$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  expect(@response).to eq 'water'
 end
 
 Then(/^I get sink$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@response).to eq 'sink'
 end
