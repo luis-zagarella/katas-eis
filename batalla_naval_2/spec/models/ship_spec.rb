b7736d7266b47801db077573ad479dfa50ff0299
luis-zagarella <zagarojo23@gmail.com>
2015-05-27 08:11:15 -0300

    subida inicial kata 04

--- batalla_naval_2/spec/models/ship_spec.rb
+++ batalla_naval_2/spec/models/ship_spec.rb
@@ -14,5 +14,16 @@ describe 'Ship' do
     expect(small.direction).to eq 'North'
     expect(small.occupied_points).to eq [@origin]
   end
-
+  
+  it 'should create a large ship in origin' do
+    large = Ship.new 'large', @origin, 'North'
+    neighbor1 = @origin.next_at('North')
+    expect(large.state).to eq 'without damage'
+    expect(large.size).to eq 2
+    expect(large.direction).to eq 'North'
+    expect(large.occupied_points.size).to eq 2
+    expect(large.occupied_points[0].is_equal(@origin)).to eq true
+    expect(large.occupied_points[1].is_equal(neighbor1)).to eq true
+  end
+  
 end
